49fd51e19c389bc5b5dccf4b7ccb6389d064f0c3
Cleverton Fernandes Guimarães <clevertonfernandesguimaraes@gmail.com>
2017-10-22 00:30:29 -0200

    Connect for every dot all possibles lines to another dot (#1)
    * Connect for every dot all possibles lines to another dot
    
    Made a refactoring in methods to extract common code for centralized method
    
    * Methods reordened to follow sequences os call in html.
    Added code to remember last function called to reexecute when apply function are called.

--- createcanvas.js
+++ createcanvas.js
@@ -3,17 +3,17 @@ function loadCanvas(id) {
     var canvas = document.createElement('canvas');
     div = document.getElementById(id);
     var context = canvas.getContext('2d');
-    canvas.id     = "TheCanvas";
-    canvas.width  = 1224;
+    canvas.id = "TheCanvas";
+    canvas.width = 1224;
     canvas.height = 768;
     //context.canvas.width  = window.innerWidth/2;
     //context.canvas.height = window.innerHeight/2;
-    canvas.style.zIndex   = 8;
+    canvas.style.zIndex = 8;
     canvas.style.position = "absolute";
-    canvas.style.border   = "4px solid";
+    canvas.style.border = "4px solid";
     //Appends Canvas do div container
     div.appendChild(canvas);
 
     //Connects 100 random dots
-    connectDots(context, selectDots(100,context), get_random_color());
+    randomDots();
 }
