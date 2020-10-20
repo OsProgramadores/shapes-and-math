3dffd0b107307a0d86862af23b2aa164aa532345
Renan <renanroberto1@gmail.com>
2020-10-20 12:50:04 -0300

    reload canvas on window resize

--- createcanvas.js
+++ createcanvas.js
@@ -1,6 +1,15 @@
+var canvasContainer = null;
+
 function loadCanvas(id) {
+    // save container id on first load
+    canvasContainer = id;
+
     //div will point to where Canvas will be created on document
     div = document.getElementById(id);
+
+    // remove old canvas if already exists
+    oldCanvas = document.getElementById("TheCanvas");
+    if (oldCanvas) div.removeChild(oldCanvas);
 
     //Creates the Canvas elemement
     var canvas = document.createElement('canvas');    
@@ -10,7 +19,7 @@ function loadCanvas(id) {
 
     canvas.id = "TheCanvas";
     canvas.width = size;
-    canvas.height = (9/16) * size;
+    canvas.height = (9/16) * size; // ration 16:9
     
     //Defines Canvas Border size
     canvas.style.border = "1px solid";
@@ -18,3 +27,8 @@ function loadCanvas(id) {
     //Appends Canvas do div container
     div.appendChild(canvas);
 }
+
+// reload canvas on window resize
+window.addEventListener("resize", function () {
+    loadCanvas(canvasContainer);
+});
